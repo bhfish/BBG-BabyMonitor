a9d4677dc19536ce8bbe304ee2a462ed53e870a5
tdun <tdun@sfu.ca>
2017-11-06 00:40:47 -0800

    Upload new file

---
+++ parent_BBG/parent_buzzer.h
@@ -0,0 +1,19 @@
+#ifndef _PARENT_BUZZER_H_
+#define _PARENT_BUZZER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "parent_gpio.h"
+#include <pthread.h>
+
+extern pthread_t buzzer_thread;
+
+int pmwBuzzOn(void);
+int pmwBuzzOff(void);
+int pmwBuzzValueSet(int period, int cycle);
+int pmwBuzzSound(int mode);
+int pmwBuzzModeDefault(void);
+int pmwBuzzLoop(void);
+int pmwBuzzInit(void);
+
+#endif
