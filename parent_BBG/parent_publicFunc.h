2b7ba20b7e18a1f1a9cf18799787395bae82fc37
tdun <tdun@sfu.ca>
2017-11-06 00:44:33 -0800

    Upload new file

---
+++ parent_BBG/parent_publicFunc.h
@@ -0,0 +1,22 @@
+#ifndef _PARENT_PUBLIC_FUNC_H_
+#define _PARENT_PUBLIC_FUNC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define STR_CONVERT(str) #str
+
+extern const struct timespec delay1s;
+extern const struct timespec delay400ns;
+extern const struct timespec delay1us;
+extern const struct timespec delay1ms;
+extern const struct timespec delay100ms;
+extern const struct timespec delay500ms;
+
+int fileWriteD(char* filePath, int value);
+int fileWriteS(char* filePath, char* value);
+int fileReadD(char* filePath);
+
+
+#endif
