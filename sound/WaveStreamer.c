1fa41eb3aa2d0dfff950cf207c8e304e3673216d
cca201 <cca201@sfu.ca>
2017-11-26 21:28:25 -0800

    fixed streaming bug where segments were getting repeated

--- sound/WaveStreamer.c
+++ sound/WaveStreamer.c
@@ -20,16 +20,17 @@ static void stopStreaming();
 static _Bool shouldStopStreaming();
 
 #define SERVER_IP "192.168.7.1"
-#define SERVER_PORT_NUMBER 1234
+#define SERVER_PORT_NUMBER 12345
 #define MAX_BYTES_PER_PACKET 16384
 #define INITIAL_NUMBER_OF_PCM_SEGMENTS 300
+#define MAX_NUMBER_OF_PCM_SEGMENTS 400
 
 static int socketFd;
 static struct sockaddr_in serverAddress;
 
 static sem_t bufferFull;
 static pthread_mutex_t bufferMutex = PTHREAD_MUTEX_INITIALIZER;
-static short* pcmSegments[INITIAL_NUMBER_OF_PCM_SEGMENTS];
+static short* pcmSegments[MAX_NUMBER_OF_PCM_SEGMENTS];
 static int pcmSegmentSize = 0;
 static int currentPcmSegment = 0;
 static int numberOfSegmentsToBuffer = 0;
@@ -70,12 +71,17 @@ void WaveStreamer_stopStreaming() {
 void WaveStreamer_setSegment(short* buffer) {
 	pthread_mutex_lock(&bufferMutex);
 	{
+		if (currentPcmSegment == MAX_NUMBER_OF_PCM_SEGMENTS) {
+			pthread_mutex_unlock(&bufferMutex);
+			return;
+		}
+
+		pcmSegments[currentPcmSegment] = malloc(pcmSegmentSize * sizeof(short));
+		memcpy(pcmSegments[currentPcmSegment], buffer, pcmSegmentSize);
+		currentPcmSegment++;
+
 		if (currentPcmSegment == numberOfSegmentsToBuffer) {
 			sem_post(&bufferFull);
-		} else {
-			pcmSegments[currentPcmSegment] = malloc(pcmSegmentSize * sizeof(short));
-			memcpy(pcmSegments[currentPcmSegment], buffer, pcmSegmentSize);
-			currentPcmSegment++;
 		}
 	}
 	pthread_mutex_unlock(&bufferMutex);
@@ -128,17 +134,21 @@ static void* waveStreamer(void* args) {
 	pthread_exit(PTHREAD_CANCELED);
 }
 
-static short** getSegments() {
-	short** segments = malloc(numberOfSegmentsToBuffer * sizeof(short *));
+static short** getSegments(int* segmentsCount) {
+	short** segments;
 	sem_wait(&bufferFull);
 	pthread_mutex_lock(&bufferMutex);
 	{
-		for (int i = 0 ; i < numberOfSegmentsToBuffer ; i++) {
+		segments = malloc(currentPcmSegment * sizeof(short *));
+
+		for (int i = 0 ; i < currentPcmSegment ; i++) {
 			segments[i] = malloc(pcmSegmentSize * sizeof(short));
 			memcpy(segments[i], pcmSegments[i], pcmSegmentSize);
+			free(pcmSegments[i]);
 		}
 
-		numberOfSegmentsToBuffer = INITIAL_NUMBER_OF_PCM_SEGMENTS / 3;
+		numberOfSegmentsToBuffer = 1;
+		*segmentsCount = currentPcmSegment;
 		currentPcmSegment = 0;
 	}
 	pthread_mutex_unlock(&bufferMutex);
@@ -147,8 +157,8 @@ static short** getSegments() {
 }
 
 static _Bool sendSegments() {
-	int segmentsCount = numberOfSegmentsToBuffer;
-	short** segments = getSegments();
+	int segmentsCount = 0;
+	short** segments = getSegments(&segmentsCount);
 	for (int i = 0 ; i < segmentsCount ; i++) {
 		sendPcmSegment(segments[i], pcmSegmentSize);
 	}
